26019653fca98fa4f0d3ee2098813adff24fac3b
Monday_David_Sarka <david_md@hamari.agency>
2022-06-25 20:35:56 +0100

    added dummy json data and a product listing page component

---
+++ data.js
@@ -0,0 +1,25 @@
+const PRODUCTS = [
+    {
+        handle: 1,
+        name: 'Women Black Shoes',
+        price: '14.99',
+        collection: 'Women',
+        image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcohenwoodworking.com%2Fwp-content%2Fuploads%2F2016%2F09%2Fimage-placeholder-500x500.jpg&f=1&nofb=1'
+    },
+    {
+        handle: 2,
+        name: 'Women Red Shoes',
+        price: '14.99',
+        collection: 'Women',
+        image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcohenwoodworking.com%2Fwp-content%2Fuploads%2F2016%2F09%2Fimage-placeholder-500x500.jpg&f=1&nofb=1'
+    },
+    {
+        handle: 3,
+        name: 'Women Purple Shoes',
+        price: '14.99',
+        collection: 'Women',
+        image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcohenwoodworking.com%2Fwp-content%2Fuploads%2F2016%2F09%2Fimage-placeholder-500x500.jpg&f=1&nofb=1'
+    },
+]
+
+export default PRODUCTS;
